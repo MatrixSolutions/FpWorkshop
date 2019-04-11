c0dd73f709ef4663ee85719439fcce5ed9530443
Michael Gilli Gilliland <mgilliland@matrixformedia.com>
2019-04-11 10:31:58 -0400

    Add step 3 answers.

--- FpWorkshop/FSharpPortion/Step03_Sequences.fs
+++ FpWorkshop/FSharpPortion/Step03_Sequences.fs
@@ -4,25 +4,25 @@ module Step03_Sequences =
 
   let run () =
     // Challenge 1 --------------------------------------
-    let anArray = [|1; 3; __|]
+    let anArray = [|1; 3; 5|]
 
     ensure (anArray = [|1; 3; 5|])
 
     // Challenge 2 --------------------------------------
-    ensure (anArray.[1] = __)
+    ensure (anArray.[1] = 3)
 
     // Challenge 3 --------------------------------------
-    let aList = [7; 11; __; 17]
+    let aList = [7; 11; 13; 17]
 
     ensure (aList = [7; 11; 13; 17])
 
     // Challenge 4 --------------------------------------
-    let aSet = Set.ofList [1; 2; 3; __]
+    let aSet = Set.ofList [1; 2; 3; 7]
 
     ensure (aSet = Set.ofList [1; 2; 3; 7])
 
     // Challenge 5 --------------------------------------
-    let aSeq = Seq.ofList [__; 100; 103]
+    let aSeq = Seq.ofList [99; 100; 103]
 
     ensure (aSeq = Seq.ofList [99; 100; 103])
 
@@ -33,11 +33,11 @@ module Step03_Sequences =
 
     // Challenge 6 --------------------------------------
     // `head` gets the first element but it's unsafe because it can throw exceptions!
-    ensure (List.head [] = 42)
+    ensure (List.head [42] = 42)
 
     // Challenge 7 --------------------------------------
     // Similar story with `tail`
-    ensure (List.tail [] = [2; 3; 4])
+    ensure (List.tail [1; 2; 3; 4] = [2; 3; 4])
 
     // NOTE: because `head` and `tail` are partial function (i.e. they don't handle
     // all possible inputs) they are considered unsafe and should be avoided in
@@ -50,11 +50,11 @@ module Step03_Sequences =
 
     let r1 = List.filter startsWithH ["hi"; "howdy"; "yo"; "sup"; "hello"]
 
-    ensure (r1 = __)
+    ensure (r1 = ["hi"; "howdy"; "hello"])
 
     // Challenge 9 --------------------------------------
     // Try it with an unnamed function (hint "not equals" is `<>` in F#)
-    let noYo = List.filter (fun x -> __) ["hi"; "howdy"; "yo"; "sup"; "hello"]
+    let noYo = List.filter (fun x -> x <> "yo") ["hi"; "howdy"; "yo"; "sup"; "hello"]
 
     let noYoExpected = ["hi"; "howdy"; "sup"; "hello"]
 
@@ -62,13 +62,13 @@ module Step03_Sequences =
 
     // Challenge 10 --------------------------------------
     // Try it with a partially applied function
-    let noYo = List.filter (__ "yo") ["hi"; "howdy"; "yo"; "sup"; "hello"]
+    let noYo = List.filter ((<>) "yo") ["hi"; "howdy"; "yo"; "sup"; "hello"]
 
     ensure (noYo = noYoExpected)
 
     // Challenge 11 --------------------------------------
     // `map` transforms all values in a container making a new container
-    let allIncremented = List.map (fun x -> __) [0; 1; 2; 3]
+    let allIncremented = List.map (fun x -> x + 1) [0; 1; 2; 3]
 
     ensure (allIncremented = [1; 2; 3; 4])
 
@@ -86,7 +86,7 @@ module Step03_Sequences =
     let double x = [x; x]
     let doubled = List.collect double [1; 2; 3]
 
-    ensure (doubled = __)
+    ensure (doubled = [1; 1; 2; 2; 3; 3])
 
     // (SPOILER) In this case, `collect` does 
     (*
@@ -102,13 +102,13 @@ module Step03_Sequences =
       ["hi"; "howdy"; "yo"; "sup"; "hello"]
         |> List.collect (fun v -> if startsWithH v then [v + "!"] else [])
 
-    ensure (exclaimOnlyStartsWithH  = __)
+    ensure (exclaimOnlyStartsWithH  = ["hi!"; "howdy!"; "hello!"])
 
     // Challenge 14 --------------------------------------
     // `fold` rolls-up the values in a collection to form a result
     let sum = List.fold (+) 0 [1; 2; 3; 4]
 
-    ensure (sum = __)
+    ensure (sum = 10)
 
     // You can think of this as building up an operation that looks like
     (*
@@ -118,14 +118,14 @@ module Step03_Sequences =
     // Challenge 15 --------------------------------------
     // the `0` was the initial value, or the value used if given an empty list
     let a1 = List.fold (+) 42 []
-    ensure (a1 = __)
+    ensure (a1 = 42)
 
-    let a2 = List.fold (+) __ [1; 2; 3]
+    let a2 = List.fold (+) 100 [1; 2; 3]
     ensure (a2 = 106)
 
     // Challenge 16 --------------------------------------
     // the result type can be different from the elements' type!
     let stringLen (x: string) = x.Length
 
-    let totalStringLen = List.fold (fun result aStr -> __) 0 ["good"; "day"; "mate"]
+    let totalStringLen = List.fold (fun result aStr -> result + stringLen aStr) 0 ["good"; "day"; "mate"]
     ensure (totalStringLen = 11)
--- FpWorkshop/FpWorkshop/Step03_Sequences.cs
+++ FpWorkshop/FpWorkshop/Step03_Sequences.cs
@@ -10,22 +10,22 @@ namespace FpWorkshop
     {
       // Challenge 1 --------------------------------------
       // What's your intuition on arrays? In other words, why use it over other enumerables?
-      var anArray = new[] {1, 3, ReplaceMe.__<int>()};
+      var anArray = new[] {1, 3, 5};
 
       Help.Ensure(anArray.SequenceEqual(new [] {1, 3, 5}));
 
       // Challenge 2 --------------------------------------
-      Help.Ensure(anArray[2] == ReplaceMe.__<int>());
+      Help.Ensure(anArray[2] == 5);
 
       // Challenge 3 --------------------------------------
       // What's your intuition on lists? In other words, why use it over other enumerables?
-      var aList = new List<int> { 7, 11, ReplaceMe.__<int>(), 17 };
+      var aList = new List<int> { 7, 11, 13, 17 };
 
       Help.Ensure(aList.SequenceEqual(new List<int> { 7, 11, 13, 17 }));
 
       // Challenge 4 --------------------------------------
       // What's your intuition on sets? In other words, why use it over other enumerables?
-      var aSet = new HashSet<int> {1, 2, 3, ReplaceMe.__<int>() };
+      var aSet = new HashSet<int> {1, 2, 3, 7 };
 
       Help.Ensure(aSet.SequenceEqual(new HashSet<int> {1, 2, 3, 7 }));
 
@@ -34,26 +34,26 @@ namespace FpWorkshop
       // What's your intuition on IEnumerables? In other words, why use it over more specific types?
       var anEnumerable = Enumerable.Range(25, 100);
 
-      Help.Ensure(anEnumerable.First() == ReplaceMe.__<int>());
+      Help.Ensure(anEnumerable.First() == 25);
 
       // NOTE: From now on we'll use IEnumerable because it's the preferred
       // type of LINQ
 
       // Challenge 6 --------------------------------------
-      Help.Ensure(new int[] { }.First() == 42);
+      Help.Ensure(new [] { 42 }.First() == 42);
 
       // Challenge 7 --------------------------------------
       // In C# you can approximate tail with Skip 1, in F# this is the same as `Seq.drop 1`
-      Help.Ensure(new int[] { }.Skip(1).SequenceEqual(new [] { 2, 3, 4 }));
+      Help.Ensure(new [] { 1, 2, 3, 4 }.Skip(1).SequenceEqual(new [] { 2, 3, 4 }));
 
       // Challenge 8 --------------------------------------
       var r1 = new[] {"hi", "howdy", "yo", "sup", "hello"}.Where(StartsWithH);
 
-      Help.Ensure(r1.SequenceEqual(ReplaceMe.__<IEnumerable<string>>()));
+      Help.Ensure(r1.SequenceEqual(new[] {"hi", "howdy", "hello"}));
 
       // Challenge 9 --------------------------------------
       var noYo = new[] {"hi", "howdy", "yo", "sup", "hello"}
-        .Where(x => ReplaceMe.__<bool>());
+        .Where(x => x != "yo");
 
       Help.Ensure(noYo.SequenceEqual(new[] { "hi", "howdy", "sup", "hello" }));
 
@@ -63,7 +63,7 @@ namespace FpWorkshop
       // Challenge 11 --------------------------------------
       // `Select` is LINQ's version of `map`
       var allIncremented = new[] {0, 1, 2, 3}
-        .Select(x => ReplaceMe.__<int>());
+        .Select(x => x + 1);
 
       Help.Ensure(allIncremented.SequenceEqual(new[] {1, 2, 3, 4}));
 
@@ -72,7 +72,7 @@ namespace FpWorkshop
       var doubled = new[] {1, 2, 3}
         .SelectMany(Double);
 
-      Help.Ensure(doubled.SequenceEqual(ReplaceMe.__<IEnumerable<int>>()));
+      Help.Ensure(doubled.SequenceEqual(new [] { 1, 1, 2, 2, 3, 3 }));
 
       // Challenge 13 --------------------------------------
       // `collect` is super powerful. It can even be used to do a sort of
@@ -81,20 +81,20 @@ namespace FpWorkshop
         new [] {"hi", "howdy", "yo", "sup", "hello" }
           .SelectMany(v => StartsWithH(v) ? new [] {v + "!"} : new string[] {});
 
-      Help.Ensure(exclaimOnlyStartsWithH.SequenceEqual(ReplaceMe.__<IEnumerable<string>>()));
+      Help.Ensure(exclaimOnlyStartsWithH.SequenceEqual(new [] {"hi!", "howdy!", "hello!" }));
 
       // Challenge 14 --------------------------------------
       // `Aggregate` is the LINQ version of fold
       var sum = new[] {1, 2, 3, 4}.Aggregate(0, (a, b) => a + b);
 
-      Help.Ensure(sum == ReplaceMe.__<int>());
+      Help.Ensure(sum == 10);
 
       // Challenge 15 --------------------------------------
       // the `0` was the initial value, or the value used if given an empty list
       var a1 = Enumerable.Empty<int>().Aggregate(0, (a, b) => a + b);
-      Help.Ensure(a1 == ReplaceMe.__<int>());
+      Help.Ensure(a1 == 0);
 
-      var a2 = new [] {1, 2, 3}.Aggregate(ReplaceMe.__<int>(), (a, b) => a + b);
+      var a2 = new [] {1, 2, 3}.Aggregate(100, (a, b) => a + b);
       Help.Ensure(a2 == 106);
 
       // Challenge 16 --------------------------------------
@@ -102,7 +102,7 @@ namespace FpWorkshop
       Func<string, int> stringLen = x => x.Length;
 
       var totalStringLen = new[] {"good", "day", "mate"}
-        .Aggregate(0, (result, aStr) => ReplaceMe.__<int>());
+        .Aggregate(0, (result, aStr) => result + stringLen(aStr));
 
       Help.Ensure(totalStringLen == 11);
     }
