3bdf1c6e4ba3b113546b7cb39f17d5529ee60614
Michael Gilli Gilliland <mgilliland@matrixformedia.com>
2019-04-11 11:43:38 -0400

    Add answers to step 4.

--- FpWorkshop/FSharpPortion/Step04_Options.fs
+++ FpWorkshop/FSharpPortion/Step04_Options.fs
@@ -18,28 +18,28 @@ module Step04_Options =
     let present = Some 42  // Kinda like [42]
     let absent = None      // Kinda like []
 
-    ensure (Option.isSome present = __)
-    ensure (Option.isSome absent = __)
+    ensure (Option.isSome present = true)
+    ensure (Option.isSome absent = false)
 
-    ensure (Option.isNone present = __)
-    ensure (Option.isNone absent = __)
+    ensure (Option.isNone present = false)
+    ensure (Option.isNone absent = true)
 
     // Challenge 2 --------------------------------------
     // If options are like lists, `Option.get` is like `List.head`
     // Also similarly, this is unsafe and should be avoided in production
     // (more to say about this later)
-    ensure (Option.get __ = 42)
+    ensure (Option.get (Some 42) = 42)
 
     // Challenge 3 --------------------------------------
     // We can transform values inside of options using `map` (sound familiar?)
 
-    ensure (Option.map (fun v -> __) (Some 43) = Some 42)
+    ensure (Option.map (fun v -> v - 1) (Some 43) = Some 42)
 
     // Challenge 4 --------------------------------------
     // If we `map` over an empty list we get the empty list. What do you think
     // mapping over `None` yields?
 
-    ensure (Option.map (fun v -> v + "!") None = __)
+    ensure (Option.map (fun v -> v + "!") None = None)
 
     // Challenge 5 --------------------------------------
     // Unfortunately (or fortunately, depending on how you look at it) options' version
@@ -47,17 +47,17 @@ module Step04_Options =
 
     // I find it fun to think of how the "squish stage" might work so let's work with that first
 
-    ensure (pretendSquish (Some None) = __)
-    ensure (pretendSquish None = __)
-    ensure (pretendSquish (Some (Some 99)) = __)
+    ensure (pretendSquish (Some None) = None)
+    ensure (pretendSquish None = None)
+    ensure (pretendSquish (Some (Some 99)) = Some 99)
 
     // Challenge 6 --------------------------------------
     // So with actual `bind`...
 
-    ensure (Option.bind (fun _ -> None)    None      = __)
-    ensure (Option.bind (fun _ -> Some 42) None      = __)
-    ensure (Option.bind (fun _ -> None)    (Some 42) = __)
-    ensure (Option.bind (fun _ -> Some 99) (Some 42) = __)
+    ensure (Option.bind (fun _ -> None)    None      = None)
+    ensure (Option.bind (fun _ -> Some 42) None      = None)
+    ensure (Option.bind (fun _ -> None)    (Some 42) = None)
+    ensure (Option.bind (fun _ -> Some 99) (Some 42) = Some 99)
 
     // SPOILER: any None makes the whole thing none!
 
@@ -73,7 +73,7 @@ module Step04_Options =
         (v1 + v2) * v3
       )))
 
-    ensure (result = __)
+    ensure (result = None)
 
     // Challenge 8 --------------------------------------
     // Why is there a `map` in the innermost part above?
@@ -83,19 +83,19 @@ module Step04_Options =
     // In lists, remember how `head` is unsafe? Well, option to the rescue!
     // `tryHead` returns an option so it's a safe alternative.
 
-    ensure (List.tryHead [] = __)
-    ensure (List.tryHead [42; 3; 9] = __)
+    ensure (List.tryHead [] = None)
+    ensure (List.tryHead [42; 3; 9] = Some 42)
 
     // Challenge 10 --------------------------------------
     // If you've been skeptical about options being similar to lists this whole time
     // Here's a little evidence!
 
-    ensure (Option.toList None = __)
-    ensure (Option.toList (Some 42) = __)
+    ensure (Option.toList None = [])
+    ensure (Option.toList (Some 42) = [42])
 
     // How'd you get in here, array!?
-    ensure (Option.toArray (Some 3) = __)
-    ensure (Option.toArray None = __)
+    ensure (Option.toArray (Some 3) = [|3|])
+    ensure (Option.toArray None = [||])
 
     // Challenge 11 --------------------------------------
     // If options are so simlar to lists then why not just use lists everywhere?
@@ -104,5 +104,5 @@ module Step04_Options =
     // Option has safer ways to get the value, like providing a default for when it's
     // not found
 
-    ensure (Option.defaultValue 42 None = __)
-    ensure (Option.defaultValue 42 (Some 99) = __)
+    ensure (Option.defaultValue 42 None = 42)
+    ensure (Option.defaultValue 42 (Some 99) = 99)
